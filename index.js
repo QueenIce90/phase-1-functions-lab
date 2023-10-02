2c2979255900f07ebcabe46b604688d9b993152e
QueenIce90 <Isamargonzalez90@gmail.com>
2023-10-02 16:49:59 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    return Math.abs(street - 42);
+} 
+
+function distanceFromHqInFeet (blocks) {
+    return distanceFromHqInBlocks (blocks)*264;
+}
+
+function distanceTravelledInFeet(street1, street2) {
+    return Math.abs(street2 - street1)*264;
+}
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet (start, destination) <= 400){
+        return 0 
+    
+    }
+    else if (distanceTravelledInFeet (start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000){
+        return (distanceTravelledInFeet (start, destination) - 400) *0.02;
+    }
+
+
+    else if (distanceTravelledInFeet (start, destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500){
+    return 25
+    }
+    else {
+        return ("cannot travel that far")
+    }
+}
